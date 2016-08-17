746cdeed76a369608d9c1c25e133c910cf7c3162
Alec Chiu <alecmchiu@gmail.com>
2016-08-17 14:36:27 -0700

    decoder bonus

--- e279.py
+++ e279.py
@@ -10,6 +10,7 @@ def encode(string,file_out):
 
 	interval_string = [string[i:i+45] for i in range(0,len(string),45)]
 
+	full_line_str = []
 	for each in interval_string:
 		full_line = ''
 		for i in range(0,len(each),3):
@@ -18,9 +19,30 @@ def encode(string,file_out):
 			groupings = [chr(int(bits[j:j+6],2)+32) for j in range(0,len(bits),6)]
 			full_line = full_line + ''.join(groupings)
 		print chr(int(len(each))+32) + full_line
+		full_line_str.append(chr(int(len(each))+32) + full_line)
 
 	print '`\nend'
+	return '\n'.join(full_line_str)
+
+def decode(string, file_out):
+	print 'begin 644 ' + file_out
+
+	lines = string.split('\n')
+	decoded_lines = []
+
+	for each in lines:
+		number_bytes = ord(each[0])-32
+		encoded = ''.join([bin(ord(char)-32)[2:].zfill(6) for char in each[1:]])
+		og_binary = [encoded[i:i+8] for i in range(0,len(encoded),8)]
+		ascii = [chr(int(x,2)) for x in og_binary]
+		decoded_lines.append(''.join(ascii))
+	print ''.join(decoded_lines)
+
+	print '`\nend'
+	return ''.join(decoded_lines)
 
 if __name__=='__main__':
 	s = 'I feel very strongly about you doing duty. Would you give me a little more documentation about your reading in French? I am glad you are happy — but I never believe much in happiness. I never believe in misery either. Those are things you see on the stage or the screen or the printed pages, they never really happen to you in life.'
-	encode(s,'file.txt')
+	e = encode(s,'file.txt')
+	decode(e,'file.txt')
+	#decode('#0V%T','cat.txt')
